07fa2034cf15b25edee74e4e19658d37f11257d2
kd2-80181 <pradyumnpawar29092000@gmail.com>
2024-02-12 22:30:00 +0530

    commit: Frontend: UserProfilePage, UserEditPage, UserService

--- frontend_onlineshopportal/ecommerce/src/App.js
+++ frontend_onlineshopportal/ecommerce/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
+import EditUser from "./customer/components/UserProfiePage/UserProfilePage";
+import AccountPage from "./customer/components/UserProfiePage/UserProfilePage";
+import UserEditProfile from "./customer/components/UserProfiePage/UserEditPage";
 import { Link, Switch, Route } from "react-router-dom";
 //import CreateUser from './customer/components/CreateAccountPage/CreateAccountPage';
 import { Router, Routes } from "react-router-dom";
@@ -16,6 +19,9 @@ function App() {
         {/* //<Route path='/' element={<HomePage/>}/> */}
         {/* <Route path='/CreateAccountPage' element={<CreateUser/>}/> */}
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/users/edituser" element={<EditUser/>}></Route>
+        <Route path="/users/accountpage" element={<AccountPage/>}></Route>
+        <Route path="/users/editaccountpage" element={<UserEditProfile/>}></Route>
         {/* //<Route path='/Navigation' element={<Navigation/>}/> */}
       </Routes>
       <Footer />
